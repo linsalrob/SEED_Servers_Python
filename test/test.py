9a868172e7fc60d1774de68f9acf5f29074c1762
Robert Edwards <raedwards@gmail.com>
2015-10-19 14:39:45 +0800

    making print work

--- test/test.py
+++ test/test.py
@@ -3,15 +3,15 @@ from servers.SAP import SAPserver
 
 server=SAPserver()
 genomes=server.all_genomes({'-complete':1, '-prokaryotic':1})
-print("There are ", len(genomes), " genomes")
+print("There are " + str(len(genomes)) + " genomes")
 
 sys.exit(0)
 for genomeID in genomes:
     sys.stderr.write("Genome: " + genomes[genomeID] + " (" + str(genomeID) + ")\n")
     prots = server.all_proteins({'-id':genomeID})
-    print("There are ", len(prots), " prots")
+    print("There are " + str(len(prots)) + " prots")
     dna   = server.ids_to_sequences({'-ids':list(prots.keys())})
-    print("There are ", len(dna), " sequences")
+    print("There are " + str(len(dna)) + " sequences")
     funcs = server.ids_to_functions({'-ids':list(prots.keys())})
-    print("There are ", len(funcs), " functions")
+    print("There are " + str(len(funcs)) + " functions")
 
